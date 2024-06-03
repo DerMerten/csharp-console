5b1504b6f898f6d365058915fd6f8747c6946596
DerMerten <90412698+DerMerten@users.noreply.github.com>
2024-06-03 11:44:45 +0200

    my last programs in c# console

---
+++ 6_ConRechner/ConRechner/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConRechner {
+    internal class Program {
+        static void Main ( string [] args ) {
+            string eingabe;
+            int result = 0;
+
+            do {
+                Console.Clear( );
+
+                Console.WriteLine("\nEinfacher Rechner" +
+                                  "\n-----------------" +
+                                  "\nBitte geben Sie +, -, *, / und x:" +
+                                  "\n(+) für addieren" +
+                                  "\n(-) für subtrahieren" +
+                                  "\n(*) für multiplizieren" +
+                                  "\n(/) für dividieren" +
+                                  "\n(x) Programm beenden");
+
+                eingabe = Console.ReadLine( );
+
+                switch (eingabe) {
+                    case "+":
+                        cacuPlus( );
+                        break;
+                    case "-":
+                        cacuMinus( );
+                        break;
+                    case "*":
+                        cacuMulti( );
+                        break;
+                    case "/":
+                        cacuDivide( );
+                        break;
+                    case "x":
+                        Console.WriteLine("Programm beendet!");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            while (eingabe != "x");
+
+            void cacuPlus () {
+                Console.Clear( );
+                Console.WriteLine("\nGeben Sie die 1. Zahl die Sie addierren wollen: ");
+                int num1 = Convert.ToInt32(Console.ReadLine( ));
+                Console.WriteLine("\nGeben Sie die 2. Zahl die Sie addierren wollen: ");
+                int num2 = Convert.ToInt32(Console.ReadLine( ));
+                result = num1 + num2;
+                Console.WriteLine("\nDas Ergebnis ist " + result);
+                Console.ReadKey( );
+                Main(null);
+            }
+
+            void cacuMinus () {
+                Console.Clear( );
+                Console.WriteLine("\nGeben Sie die 1. Zahl die Sie subtrahieren wollen: ");
+                int num1 = Convert.ToInt32(Console.ReadLine( ));
+                Console.WriteLine("\nGeben Sie die 2. Zahl die Sie subtrahieren wollen: ");
+                int num2 = Convert.ToInt32(Console.ReadLine( ));
+                result = num1 - num2;
+                Console.WriteLine("\nDas Ergebnis ist " + result);
+                Console.ReadKey( );
+                Main(null);
+            }
+
+            void cacuMulti () {
+                Console.Clear( );
+                Console.WriteLine("\nGeben Sie die 1. Zahl die Sie multiplizieren wollen: ");
+                int num1 = Convert.ToInt32(Console.ReadLine( ));
+                Console.WriteLine("\nGeben Sie die 2. Zahl die Sie multiplizieren wollen: ");
+                int num2 = Convert.ToInt32(Console.ReadLine( ));
+                result = num1 * num2;
+                Console.WriteLine("\nDas Ergebnis ist " + result);
+                Console.ReadKey( );
+                Main(null);
+            }
+
+            void cacuDivide () {
+                Console.Clear( );
+                Console.WriteLine("\nGeben Sie die 1. Zahl die Sie dividieren wollen: ");
+                int num1 = Convert.ToInt32(Console.ReadLine( ));
+                Console.WriteLine("\nGeben Sie die 2. Zahl die Sie dividieren wollen: ");
+                int num2 = Convert.ToInt32(Console.ReadLine( ));
+                result = num1 / num2;
+                Console.WriteLine("\nDas Ergebnis ist " + result);
+                Console.ReadKey( );
+                Main(null);
+            }
+
+
+        }
+    }
+}
---
+++ 7_ConString/ConString/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConString {
+    internal class Program {
+        static void Main ( string [] args ) {
+
+            //umgedrehtString( );
+            //varName( );
+            //dataParameter( );
+            urlTopLevel( );
+
+
+
+
+            void umgedrehtString () {
+                Console.Clear( );
+                Console.Write("\nGeben Sie eine Zeichenfolge ein: ");
+                string input = Console.ReadLine( );
+                char [] charsInput = input.ToCharArray( );
+                Array.Reverse(charsInput);
+                string output = new string(charsInput);
+                Console.WriteLine("\nUmgedrehter Text: " + output);
+                Console.ReadKey( );
+            }
+
+            void varName () {
+                Console.Clear( );
+                Console.Write("\nGeben Sie Ihren Vor- und Nachnamen und trennen Sie dies durch einen Komma: ");
+                string input = Console.ReadLine( );
+                string [] vollName = input.Split(',');
+                if (vollName.Length == 2) {
+                    string name = vollName [ 0 ].Trim( );
+                    string vorname = vollName [ 1 ].Trim( );
+
+                    string initial = name [ 0 ].ToString( ).ToLower( ) + vorname [ 0 ].ToString( ).ToLower( );
+
+                    Console.WriteLine("\nName: " + name);
+                    Console.WriteLine("\nVorname: " + vorname);
+                    Console.WriteLine("\nInitialen: " + initial);
+                }
+                Console.ReadKey( );
+            }
+
+            void dataParameter () {
+                Console.Clear( );
+                Console.Write("\nGeben Sie den Dateiname ein: ");
+                string input = Console.ReadLine( );
+                Console.WriteLine(input + ".bak");
+                Console.ReadKey( );
+            }
+
+            void urlTopLevel () {
+                Console.Clear( );
+                Console.Write("\nGeben Sie eine URL ein: ");
+                string eingabe = Console.ReadLine( );
+
+                if (!eingabe.StartsWith("http://%22/") && !eingabe.StartsWith("https://%22%29/")) {
+                    eingabe = "http://" + eingabe;
+                }
+                Uri uri = new Uri(eingabe);
+
+                string topLevelDomain = uri.GetLeftPart(UriPartial.Authority);
+
+                Console.WriteLine("\nTop-Level-Domain: " + topLevelDomain);
+                Console.ReadKey( );
+            }
+        }
+    }
+}
